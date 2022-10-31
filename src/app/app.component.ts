7774633750f97b0af43cc98925a3392614b0548c
Alison Rafael <armg.alison@gmail.com>
2022-10-31 17:58:26 -0300

    updated build config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { ToastService } from '@services/toast.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(public toasterService: ToastService) {}
-}
+export class AppComponent {}
